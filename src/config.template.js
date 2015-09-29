a249926382b7342203e864c42c56cae328bcb851
Travis Tidwell <codertravist@gmail.com>
2015-09-29 13:19:11 -0500

    Added the app to be used with open source.

--- src/config.template.js
+++ src/config.template.js
@@ -1,4 +1,9 @@
 angular.module('formioApp').constant('AppConfig', {
+{% if domain %}
+  appUrl: '{{ domain }}',
+  apiUrl: '{{ domain }}'
+{% else %}
   appUrl: '{{ protocol }}://{{ path }}.{{ host }}',
   apiUrl: '{{ protocol }}://api.{{ host }}'
+{% endif %}
 });
