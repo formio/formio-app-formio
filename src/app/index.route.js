2b152dc84a883fe0c2cf12580f838a88c2fb0c00
Guillaume Smaha <guillaume.smaha@gmail.com>
2017-03-28 16:09:26 -0400

    Add search fields in home page

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -16,12 +16,32 @@
         url: '/',
         templateUrl: 'views/main.html',
         controller: ['$scope', function($scope) {
+          $scope.searchTypes = [
+            {
+              name: 'name',
+              title: 'Name'
+            },
+            {
+              name: 'title',
+              title: 'Title'
+            },
+            {
+              name: 'tags',
+              title: 'Tags'
+            }
+          ];
           $scope.resources = [];
           $scope.resourcesUrl = AppConfig.appUrl + '/form?type=resource';
+          $scope.resourcesUrlParams = {};
           $scope.resourcesLoading = true;
+          $scope.resourcesSearch = '';
+          $scope.resourcesSearchType = 'name';
           $scope.forms = [];
           $scope.formsUrl = AppConfig.appUrl + '/form?type=form';
+          $scope.formsUrlParams = {};
           $scope.formsLoading = true;
+          $scope.formsSearch = '';
+          $scope.formsSearchType = 'name';
           $scope.formsPerPage = 5;
           $scope.$on('pagination:loadPage', function (event, status, config) {
             if (config.url.indexOf('type=resource') !== -1) {
@@ -31,6 +51,22 @@
               $scope.formsLoading = false;
             }
           });
+          $scope.updateResourceSearch = function() {
+            var params = {};
+            if ($scope.resourcesSearch.length > 0) {
+              var paramName = $scope.resourcesSearchType+'__regex';
+              params[paramName] = '/'+$scope.resourcesSearch+'/i';
+            }
+            $scope.resourcesUrlParams = params;
+          };
+          $scope.updateFormSearch = function() {
+            var params = {};
+            if ($scope.formsSearch.length > 0) {
+              var paramName = $scope.formsSearchType+'__regex';
+              params[paramName] = '/'+$scope.formsSearch+'/i';
+            }
+            $scope.formsUrlParams = params;
+          };
         }]
       });
 
